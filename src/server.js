edbda48ab7ecd9d02247a27566399cfe6b945cc5
simonryrie <simonryrie@gmail.com>
2023-05-12 16:52:45 +0100

    Challenge 3 completed

--- src/server.js
+++ src/server.js
@@ -7,17 +7,22 @@ server.get("/", (request, response) => {
 });
 
 server.get("/colour", (request, response) => {
-  const hex = request.query.hex || "ffffff"; // default to white if hex is not present
+  let hex = request.query.hex || "ffffff"; // default to white if hex is not present
   response.send(`
     <!DOCTYPE html>
     <html>
       <head>
         <style>
-          body { background-color: #${hex}; }
+          body { background-color: ${hex}; }
         </style>
       </head>
       <body>
-        <h1>Colour: #${hex}</h1>
+        <h1>Colour: ${hex}</h1>    
+        <form method="get" action="/colour">
+            <label for="color-input">Enter a hex color code:</label>
+            <input type="text" id="color-input" name="hex" pattern="#[0-9a-fA-F]{6}" required>
+            <button type="submit">Submit</button>
+        </form>
       </body>
     </html>
   `);
